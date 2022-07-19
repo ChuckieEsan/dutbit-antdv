26198986c453fca5eef024f7acb545b41414a268
Flinx-LY <38970229+Flinx-LY@users.noreply.github.com>
2022-07-19 19:22:21 +0800

    dev

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ const app = createApp(App)
 app.use(router)
 app.use(VueAxios, axios)
 if (import.meta.env.DEV) {
-  app.provide('axiosBaseURL', 'https://www.dutbit.com/apivue')
+  app.provide('axiosBaseURL', 'https://vm.dutbit.com/apivue')
 } else {
   app.provide('axiosBaseURL', 'https://www.dutbit.com/apivue')
 }
